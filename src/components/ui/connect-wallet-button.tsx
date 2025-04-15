945b9c695a86fc8a211fde2932183cd815741854
Matt <44452452+monghaii@users.noreply.github.com>
2025-04-15 06:17:38 -0700

    Update connect-wallet-button.tsx

--- src/components/ui/connect-wallet-button.tsx
+++ src/components/ui/connect-wallet-button.tsx
@@ -5,7 +5,7 @@ const ConnectWalletButton = () => {
   return (
     <>
       <Button className="bg-pink-700/20 hover:bg-pink-600/25 w-full py-6 text-lg text-pink-500 rounded-full cursor-pointer mt-1">
-        Conect Wallet
+        Connect Wallet
       </Button>
     </>
   )
